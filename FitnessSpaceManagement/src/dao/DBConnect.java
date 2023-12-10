85ba184d71c7267972704e17583f556dbc76892f
rahulnagaraju724 <rahulnagaraju724@gmail.com>
2023-12-09 19:44:57 -0600

    Changing database from 510labs to 510fp

--- FitnessSpaceManagement/src/dao/DBConnect.java
+++ FitnessSpaceManagement/src/dao/DBConnect.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnect  {
-
+	
 	protected Connection connection;
 	public Connection getConnection() { return connection; }
 
@@ -18,6 +18,7 @@ public class DBConnect  {
 		
 		try {
 			connection = DriverManager.getConnection(url, username, password);
+			System.out.printlm("Successfully connected to database!!");
 		} catch (SQLException e) {
 			System.out.println("Error creating connection to database: " + e);
 			System.exit(-1);
